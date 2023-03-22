ba44d9ab6549566518e76909385330ccfbad7cef
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-03-22 21:04:32 +0530

    feat : frontend client added to repository

---
+++ frontend/App.tsx
@@ -0,0 +1,222 @@
+import {
+  AuthBindings,
+  Authenticated,
+  GitHubBanner,
+  Refine,
+} from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import {
+  ErrorComponent,
+  Layout,
+  RefineSnackbarProvider,
+  notificationProvider,
+} from "@refinedev/mui";
+
+import { CssBaseline, GlobalStyles } from "@mui/material";
+import routerBindings, {
+  CatchAllNavigate,
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import dataProvider from "@refinedev/simple-rest";
+import axios, { AxiosRequestConfig } from "axios";
+import { CredentialResponse } from "interfaces/google";
+import { Login } from "pages/login";
+import {
+  ProductCreate,
+  ProductEdit,
+  ProductList,
+  ProductShow,
+} from "pages/products";
+import { useTranslation } from "react-i18next";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { parseJwt } from "utils/parse-jwt";
+import { Header } from "./src/components/layouts/header";
+import { ColorModeContextProvider } from "./src/contexts/color-mode";
+import { Sider, Title } from "./src/components/layouts";
+
+const axiosInstance = axios.create();
+axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
+  const token = localStorage.getItem("token");
+  if (request.headers) {
+    request.headers["Authorization"] = `Bearer ${token}`;
+  } else {
+    request.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  return request;
+});
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const authProvider: AuthBindings = {
+    login: async ({ credential }: CredentialResponse) => {
+      const profileObj = credential ? parseJwt(credential) : null;
+
+      if (profileObj) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            ...profileObj,
+            avatar: profileObj.picture,
+          })
+        );
+
+        localStorage.setItem("token", `${credential}`);
+
+        return {
+          success: true,
+          redirectTo: "/",
+        };
+      }
+
+      return {
+        success: false,
+      };
+    },
+    logout: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token && typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        axios.defaults.headers.common = {};
+        window.google?.accounts.id.revoke(token, () => {
+          return {};
+        });
+      }
+
+      return {
+        success: true,
+        redirectTo: "/login",
+      };
+    },
+    onError: async (error) => {
+      console.error(error);
+      return { error };
+    },
+    check: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        return {
+          authenticated: true,
+        };
+      }
+
+      return {
+        authenticated: false,
+        error: new Error("Not authenticated"),
+        logout: true,
+        redirectTo: "/login",
+      };
+    },
+    getPermissions: async () => null,
+    getIdentity: async () => {
+      const user = localStorage.getItem("user");
+      if (user) {
+        return JSON.parse(user);
+      }
+
+      return null;
+    },
+  };
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <CssBaseline />
+          <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+          <RefineSnackbarProvider>
+            <Refine
+              dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+              notificationProvider={notificationProvider}
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              i18nProvider={i18nProvider}
+              resources={[
+                {
+                  name: "products",
+                  list: "/products",
+                  create: "/products/create",
+                  edit: "/products/edit/:id",
+                  show: "/products/show/:id",
+                  meta: {
+                    canDelete: true,
+                  },
+                },
+              ]}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+              }}
+            >
+              <Routes>
+                <Route
+                  element={
+                    <Authenticated fallback={<CatchAllNavigate to="/login" />}>
+                      <Layout Header={Header}>
+                        <Outlet />
+                      </Layout>
+                      <Layout Sider={Sider}>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route
+                    index
+                    element={<NavigateToResource resource="products" />}
+                  />
+                  <Route path="/products">
+                    <Route index element={<ProductList />} />
+                    <Route path="create" element={<ProductCreate />} />
+                    <Route path="edit/:id" element={<ProductEdit />} />
+                    <Route path="show/:id" element={<ProductShow />} />
+                  </Route>
+                </Route>
+                <Route
+                  element={
+                    <Authenticated fallback={<Outlet />}>
+                      <NavigateToResource />
+                    </Authenticated>
+                  }
+                >
+                  <Route path="/login" element={<Login />} />
+                </Route>
+                <Route
+                  element={
+                    <Authenticated>
+                      <Layout Header={Header}>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route path="*" element={<ErrorComponent />} />
+                </Route>
+              </Routes>
+
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+            </Refine>
+          </RefineSnackbarProvider>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,52 @@
+import { useLogin } from "@refinedev/core";
+import { useEffect, useRef } from "react";
+import { CredentialResponse } from "../interfaces/google";
+
+export const Login: React.FC = () => {
+  const { mutate: login } = useLogin<CredentialResponse>();
+
+  const GoogleButton = (): JSX.Element => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      if (typeof window === "undefined" || !window.google || !divRef.current) {
+        return;
+      }
+
+      try {
+        window.google.accounts.id.initialize({
+          ux_mode: "popup",
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          callback: async (res: CredentialResponse) => {
+            if (res.credential) {
+              login(res);
+            }
+          },
+        });
+        window.google.accounts.id.renderButton(divRef.current, {
+          theme: "filled_blue",
+          size: "medium",
+          type: "standard",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+    return <div ref={divRef} />;
+  };
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#212121",
+      }}
+    >
+      <GoogleButton />
+    </div>
+  );
+};
